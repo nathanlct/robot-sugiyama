04eff86894d303c748e55d214bbe27644a5e59f2
nathanlct <nathanlct@icloud.com>
2019-07-02 17:33:28 -0700

    better controllers and camera

---
+++ camera.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(1)
+    time.sleep(1.00)
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=False)
+
+
+if __name__ == '__main__':
+    main()
